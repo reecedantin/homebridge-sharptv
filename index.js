283a49177548c2b8ee746a44bd779f14077a9884
reecedantin <reecedantin@yahoo.com>
2017-02-10 22:43:23 -0500

    get now checks if err before closing

--- index.js
+++ index.js
@@ -93,10 +93,7 @@ SharpTVAccessory.prototype.getState = function(callback) {
 
   client.on('data', function(data) {
       state = data.toString('utf-8').trim();
-      accessory.log("GOT " + state);
-      if(state == "ERR") {
-          client.write(command + '\r\n');
-      } else {
+      if(state !== "ERR") {
           client.end();
       }
   });
