9baf7478774f6129150e0d4e5c7de3c943b3a276
reecedantin <reecedantin@yahoo.com>
2017-02-10 22:31:46 -0500

    i dropped this =

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ SharpTVAccessory.prototype.getState = function(callback) {
 
   client.on('data', function(data) {
       state = data.toString('utf-8').trim();
-      if(state = "ERR") {
+      if(state == "ERR") {
           client.write(command + '\r\n');
       } else {
           client.end();
